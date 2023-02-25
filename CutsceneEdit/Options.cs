c4e75b2392592408e542c79a5d1615d9e9d0bb9d
GompDS <79070939+GompDS@users.noreply.github.com>
2023-02-25 11:13:47 -0800

    instructions

--- CutsceneEdit/Options.cs
+++ CutsceneEdit/Options.cs
@@ -1,6 +1,81 @@
+using System.Numerics;
+
 namespace CutsceneEdit;
 
 public class Options
 {
+    public enum EditType
+    {
+        Change,
+        Set,
+        None
+    }
     
+    public EditType RotateType = EditType.None;
+
+    public EditType MoveType = EditType.None;
+    public Vector3 VectorChange { get; }
+
+    public int CutIndex = -1;
+    public Options(string[] args)
+    {
+        if (args[0].Length != 2) throw new IOException("Incorrect arguments given.");
+        if (args[0][0] == 'r')
+        {
+            RotateType = args[0][1] switch
+            {
+                'c' => EditType.Change,
+                's' => EditType.Set,
+                _ => EditType.None
+            };
+        }
+        else if (args[0][0] == 'm')
+        {
+            MoveType = args[0][1] switch
+            {
+                'c' => EditType.Change,
+                's' => EditType.Set,
+                _ => EditType.None
+            };
+        }
+        else
+        {
+            throw new IOException("Must choose to rotate or move.");
+        }
+
+        Vector3 vectorChange = VectorChange;
+        if (float.TryParse(args[1], out float valueX))
+        {
+            vectorChange.X = valueX;
+        }
+        else
+        {
+            vectorChange.X = 0;
+        }
+        
+        if (float.TryParse(args[2], out float valueY))
+        {
+            vectorChange.Y = valueY;
+        }
+        else
+        {
+            vectorChange.Y = 0;
+        }
+        
+        if (float.TryParse(args[3], out float valueZ))
+        {
+            vectorChange.Z = valueZ;
+        }
+        else
+        {
+            vectorChange.Z = 0;
+        }
+        
+        if (int.TryParse(args[4], out int cutIndexValue))
+        {
+            CutIndex = cutIndexValue;
+        }
+
+        VectorChange = vectorChange;
+    }
 }
--- CutsceneEdit/Program.cs
+++ CutsceneEdit/Program.cs
@@ -1,2 +1,146 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Numerics;
+using SoulsFormats;
+
+namespace CutsceneEdit;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Options op = new (args);
+        ISoulsFile backup;
+        string? filePath = args.FirstOrDefault(x => File.Exists(x));
+        if (filePath == null)
+        {
+            throw new IOException("No BND4 or MQB given in arguments.");
+        }
+        if (BND4.IsRead(filePath, out BND4 bnd))
+        {
+            backup = BND4.Read(filePath);
+            BinderFile? file = bnd.Files.FirstOrDefault(x => MQB.Is(x.Bytes));
+            if (file != null)
+            {
+                MQB cutscene = MQB.Read(file.Bytes);
+                cutscene.EditCutscene(op);
+                file.Bytes = cutscene.Write();
+            }
+
+            bnd.Write(filePath);
+        }
+        else if (MQB.IsRead(filePath, out MQB cutscene))
+        {
+            backup = MQB.Read(filePath);
+            cutscene.EditCutscene(op);
+            cutscene.Write(filePath);
+        }
+        else
+        {
+            return;
+        }
+
+        if (!File.Exists($"{filePath}.bak"))
+        {
+            backup.Write($"{filePath}.bak");
+        }
+    }
+
+    public static void EditCutscene(this MQB cutscene, Options op)
+    {
+        if (op.RotateType == Options.EditType.Change)
+        {
+            cutscene.ChangeCutsceneRotation(op.VectorChange.X, op.VectorChange.Y, op.VectorChange.Z, op.CutIndex);
+        }
+        else if (op.RotateType == Options.EditType.Set)
+        {
+            cutscene.SetCutsceneRotation(op.VectorChange.X, op.VectorChange.Y, op.VectorChange.Z, op.CutIndex);
+        }
+        
+        if (op.MoveType == Options.EditType.Change)
+        {
+            cutscene.ChangeCutscenePosition(op.VectorChange.X, op.VectorChange.Y, op.VectorChange.Z, op.CutIndex);
+        }
+        else if (op.MoveType == Options.EditType.Set)
+        {
+            cutscene.SetCutscenePosition(op.VectorChange.X, op.VectorChange.Y, op.VectorChange.Z, op.CutIndex);
+        }
+    }
+
+    public static void ChangeCutscenePosition(this MQB cutscene, float changeX, float changeY, float changeZ, int cutIndex)
+    {
+        List<MQB.Transform> transforms = new();
+        
+        if (cutIndex > -1)
+        {
+            transforms.AddRange(cutscene.Cuts[cutIndex].Timelines.SelectMany(y => y.Dispositions
+                .SelectMany(z => z.Transforms)));
+        }
+        else
+        {
+            transforms.AddRange(cutscene.Cuts.Where(x => x.Duration >= 0)
+                .SelectMany(x => x.Timelines.SelectMany(y => y.Dispositions
+                    .SelectMany(z => z.Transforms))));
+        }
+
+        foreach (MQB.Transform transform in transforms)
+        {
+            transform.Translation = new Vector3(transform.Translation.X + changeX, transform.Translation.Y + changeY, transform.Translation.Z + changeZ);
+        }
+    }
+
+    public static void SetCutscenePosition(this MQB cutscene, float newX, float newY, float newZ, int cutIndex)
+    {
+        if (cutIndex < 0)
+        {
+            throw new ArgumentException("You can only set the position of one cut at a time. Make sure to specify the cut index.");
+        }
+        foreach (MQB.Transform transform in cutscene.Cuts[cutIndex].Timelines.SelectMany(y => y.Dispositions
+                         .SelectMany(z => z.Transforms)))
+        {
+            transform.Translation = new Vector3(newX, newY, newZ);
+        }
+    }
+    
+    public static void ChangeCutsceneRotation(this MQB cutscene, float changeX, float changeY, float changeZ, int cutIndex)
+    {
+        List<MQB.Transform> transforms = new();
+        
+        if (cutIndex > -1)
+        {
+            transforms.AddRange(cutscene.Cuts[cutIndex].Timelines.SelectMany(y => y.Dispositions
+                .SelectMany(z => z.Transforms)));
+        }
+        else
+        {
+            transforms.AddRange(cutscene.Cuts.Where(x => x.Duration >= 0)
+                .SelectMany(x => x.Timelines.SelectMany(y => y.Dispositions
+                    .SelectMany(z => z.Transforms))));
+        }
+
+        foreach (MQB.Transform transform in transforms)
+        {
+            transform.Rotation = new Vector3(transform.Rotation.X + changeX, transform.Rotation.Y + changeY, transform.Rotation.Z + changeZ);
+        }
+    }
+    
+    public static void SetCutsceneRotation(this MQB cutscene, float newX, float newY, float newZ, int cutIndex)
+    {
+        List<MQB.Transform> transforms = new();
+        
+        if (cutIndex > -1)
+        {
+            transforms.AddRange(cutscene.Cuts[cutIndex].Timelines.SelectMany(y => y.Dispositions
+                .SelectMany(z => z.Transforms)));
+        }
+        else
+        {
+            transforms.AddRange(cutscene.Cuts.Where(x => x.Duration >= 0)
+                .SelectMany(x => x.Timelines.SelectMany(y => y.Dispositions
+                    .SelectMany(z => z.Transforms))));
+        }
+
+        foreach (MQB.Transform transform in transforms)
+        {
+            transform.Rotation = new Vector3(newX, newY, newZ);
+        }
+    }
+}
